0288be088dd33edd520e9e77dd64ad05bb06e2e3
Chris Moore <chris@cloudspace.com>
2015-03-05 17:51:35 -0500

    compiled .go go code added to .go

--- go-strlen.go
+++ go-strlen.go
@@ -1,5 +1,15 @@
 package main
 
-import()
+import(
+      "fmt"
+      "unicode/utf8"
+      "encoding/json"
+      "os"
+)
 
-func main() {}
+func main() {
+  byte_map := map[string]int{"length" : utf8.RuneCountInString(os.Args[1])}
+
+  result, _ := json.Marshal(byte_map)
+  fmt.Println(string(result))
+}
